55c3d29b1678c3b4324e10c56dfef1e9825ddb3e
Houshalter-server <Houshalter@gmail.com>
2014-12-18 04:03:20 -0500

    finished putting scraping in it's own function.

--- get_data.lua
+++ get_data.lua
@@ -2,17 +2,16 @@ http = require "socket.http"
 cjson = require "cjson"
 http.USERAGENT = "wordvis comment scraper by /u/Noncomment"
 
---start = os.time()
-
 function scrapeComments(numComments)
 	local dataset = {}
 	local seen = {}
 	local nextId = ""
-	local oldcomments = 0
+	local oldcomments, done = 0, false
 	while true do
 		local res, sts = http.request("http://www.reddit.com/comments.json?limit=100"..nextId)
 		if sts == 200 then
-			local children, done = cjson.decode(res).data.children
+			local children = cjson.decode(res).data.children
+			done = false
 			for k, v in ipairs(children) do
 				if not seen[v.data.id] then seen[v.data.id] = true
 				else done = true break end
@@ -24,14 +23,17 @@ function scrapeComments(numComments)
 			oldcomments = #dataset
 		else print("error status "..sts) end 
 		print("\n\n\n"..dataset[#dataset])
-		if #dataset > numComments
-		socket.sleep(10)
-		print("\n"..((nextId:len()>0) and "Going to next page." or "Loading first page."))
+		if #dataset >= numComments then break end
+		socket.sleep(done and 10 or 2)
+		print("\n"..(done and "Going to next page." or "Loading first page."))
 	end
-
+	return dataset
 end
 
---[[seconds = os.difftime(os.time(), start)
+start = os.time()
+local dataset = scrapeComments(1000)
+
+seconds = os.difftime(os.time(), start)
 total = 0
 for k, v in ipairs(dataset) do
 	total = total + v:len()
@@ -41,7 +43,7 @@ average = total/#dataset
 print(string.format([[%i comments harvested in %i seconds. Or %.3f comments per second.
 An average of %.2f characters per comment. %i characters total.
 %.3f characters per second. %.3f words per second at 5 characters per word.]],
-#dataset, seconds, #dataset/seconds, average, total, total/seconds, total/seconds/5))]]
+#dataset, seconds, #dataset/seconds, average, total, total/seconds, total/seconds/5))
 
 --[[14969 comments harvested in 2918 seconds. Or 5.130 comments per second.
 An average of 162.85 characters per comment. 2437715 characters total.
