b3e76f9d3fcfc6574d183b9f0cb6382e57e54176
Houshalter-server <Houshalter@gmail.com>
2014-12-19 09:12:18 -0500

    minor edits and working on main

--- main.lua
+++ main.lua
@@ -9,11 +9,27 @@
 --get the associated word vector for every word and build a table of these for each sentence
 --if the word is new, assign it a random vector
 --and high step size and few online updates
-l = require 'lpeg'
+L = require 'lpeg'
 dofile "get_data.lua"
 
+l = L.locale(l)
+
 function seperateWords(str)
-	for k, m in str:gmatch("(%a+)")
+	local rawElements = {}
+	for k,v in str:lower():gmatch("%s?([^%s]+)%s?") do
+		table.insert(rawElements,v)
+	end
+	local elements = {}
+	--add only words that don't contain non-letters
+	for k,v in ipairs(rawElements) do
+		if not v:find("[^%a]") then
+			table.insert(elements, v)
+		end
+	end
+	
+	--[[string.find(str, "%a+")
+	a = (l.space^0*C(l.alpha^1)*L.S('.',',',"'s","'")^0*l.space^0)
+	b = a*b]]
 end
 
 comments = scrapeComments(100)
